9bf1490354acafbefa962ae8cd41d70d319f7a09
mandylee900125 <leem3209@gmail.com>
2016-09-23 10:23:36 -0700

    changes

---
+++ Lesson_03/Lab_03.py
@@ -0,0 +1,29 @@
+
+
+
+
+print ("Hi, my name is RudeAi.")
+print ("I'd like to ask you some questions.")
+
+name = input("What is your name?")
+print (name, "?!! What would anyone name a baby that?")
+
+age = int(print("How old are you?"))
+print ("Wow ", age, " is getting up there.")
+
+hobby = print("What do you do for fun?")
+print (hobby, "? Thay's so boring!")
+
+music = print ("What music do you like?")
+print ("Boo! I wouldn't wish the sound of ", music, " is my worst enemy.")
+
+siblings = int(print("How many siblings do you have?")
+print (siblings, "?, I hope your rest of your family is better looking.")
+
+job = print("What do you want to be when you grow up?")
+print ("I think you'd to be smarter to be a ", job, ".")
+
+print ("So ",name, " you're ",age ,"...")
+print ("You like to ", hobby," and listen to ", music, "...")
+print ("Have fun being a ", job, ".")
+print ("I'm only kidding " ,name, ".")
