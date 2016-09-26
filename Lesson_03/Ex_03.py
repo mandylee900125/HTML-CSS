18ce7a6ecf5fd0d1f2b7d0b451af9a8ec6fd5509
mandylee900125 <leem3209@gmail.com>
2016-09-26 09:59:30 -0700

    added

---
+++ Lesson_03/Ex_03.py
@@ -0,0 +1,5 @@
+width = int(input("Enter the width of your computer in centimeter."))
+length = int(input("Enter the length of your computer in centimeter."))
+height = int(input("Enter the height of your computerin centimeter."))
+volume = width* length* height
+print ("The volume of your computer is ", volume, " cm cube.")
