2e88c9cd3df493084de2148385e01e73dac77aa6
mandylee900125 <leem3209@gmail.com>
2016-09-29 09:55:35 -0700

    fixed

--- Lesson_02/Lab_02.py
+++ Lesson_02/Lab_02.py
@@ -4,15 +4,20 @@ num2 = 10
 num3 = num1* num2
 
 print (num1, " multiplied ", num2, " is ", num3)
+print ("\t")
 
-name = "Paper"
+
+name = "Sheep"
 address = "1234 Hello"
 city = "San Diego"
 zipCode = 92130
+state = "CA "
 
-print ("Your name is: ", name)
-print ("Address :",address, ",",city, ",",zipCode)
+print (name)
+print (address)
+print (city,",",state,zipCode)
 
+print ("\t")
 l = 3
 w = 5
 area = l * 3 
