0852b272e9136442e2804f963bc79c35b46cd7f1
mandylee900125 <leem3209@gmail.com>
2016-10-05 10:25:01 -0700

    changes

--- Lesson_04/Ex_04.py
+++ Lesson_04/Ex_04.py
@@ -1,1 +1,14 @@
-print
+def main(h,l,w):
+    h = int(input("Enter the height in inches:"))
+    l = int(input("Enter the length in inches:"))
+    w = int(input("Enter the width in inches:"))
+    print ("The volume of your subwoofer box is", calcVol(h,l,w),"cubic feet")
+
+def calcVol(h,l,w):
+    volume = (h*12)*(l*12)*(w*12)
+    return volume
+
+ print ("The volume of your subwoofer box is", calcVol(h,l,w),"cubic feet")
+
+
+
