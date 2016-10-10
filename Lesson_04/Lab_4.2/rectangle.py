13c3fdc45118ed1ca4e42c54ffde4234aa086ea1
mandylee900125 <leem3209@gmail.com>
2016-10-10 09:42:33 -0700

    changes

--- Lesson_04/Lab_4.2/rectangle.py
+++ Lesson_04/Lab_4.2/rectangle.py
@@ -1,10 +1,11 @@
 length = float(input("Enter the length:"))
 width = float(input("Enter the width:"))
 
-calcPerim (length,width)
+def calcPerim(length,width):
     perimeter = "{:10.5f}".format((length + width)*2)
     return perimeter
-print()
-    output = "Your rectangle is", calcPerim (length,width), "ft around."
+def printl():
+    print("Your rectangle is", calcPerim (length,width), "ft around.")
 
-    
+
+printl()    
