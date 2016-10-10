9520c83aae46ae1a4f6a07b6263e3447906e52ea
mandylee900125 <leem3209@gmail.com>
2016-10-10 10:22:26 -0700

    changes

---
+++ Lesson_04/Lab_4.2/circle.py
@@ -0,0 +1,9 @@
+r = float(input("Enter the radius:"))
+
+def calcArea(r):
+    area = r*r*3.14
+    return area
+def printl(r):
+    print("The area of a circle with a radius of" ,r, "is" ,calcArea(r),".")
+
+printl(r)
