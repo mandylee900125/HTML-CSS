255177255061f0705642809d13dc560e8b708cdd
mandylee900125 <leem3209@gmail.com>
2016-10-12 09:35:55 -0700

    changes

--- Lesson_04/Lab_4.3/circle.py
+++ Lesson_04/Lab_4.3/circle.py
@@ -1,10 +1,8 @@
-global area
-global r
 r = float(input("Enter the radius:"))
 
-
 def calcArea():
-    area = ((r*r)*3.14)
+    global area
+    area ="{:6.5}".format((r*r)*3.14)
 
 def printl():
     print("The area of a circle with a radius of" ,r, "is" ,area,".")
