aa8a8f732f1174aebced5b1776d8cc336119d810
mandylee900125 <LeeM3209@sduhsd.lan>
2016-10-17 10:25:11 -0700

    chnages

---
+++ Lesson_05/Lab_01/Ex_02.py
@@ -0,0 +1,25 @@
+item1 = input("Please enter item 1:"))
+price1 = float(input("Please enter the price for item 1:"))
+item2 = input("Please enter item 2:"))
+price2 = float(input("Please enter the price for item 2:"))
+item3 = input("Please enter item 3:"))
+price3 = float(input("Please enter the price for item 3:"))
+item4 = input("Please enter item 4:"))
+price4 = float(input("Please enter the price for item 4:"))
+subtotal = price1+ price2+ price3+ price4
+tax = subtotal* 0.075
+total = subtotal- discount+ tax
+
+def discount():
+    if subtotal >= 2000
+        discount = subtotal*0.15
+    if subtotal <= 2000
+        discount = 0
+        
+def format(item,price):
+    print("*{:>15}.....{:>10.2f}".format(item,price)
+      
+def format():
+    print("<<<<<<<<<<< Receipt >>>>>>>>>>>>>>")
+    print("*{:>15}.....{:>10.2f}" format.(item,price))
+    
--- Study Guide/Study Guide.py
+++ Study Guide/Study Guide.py
@@ -1,12 +1,16 @@
-area = 0
-radius = 6
-
-def calcRadius():
-    global area
-    area = 3.14*(radius**2)
-
-def radPrint():
-    print("The radius of your circle is{:0.3f}".format(area))
-calcRadius()
-radPrint()
-
+#1
+#print("The total cost of yout order is ${:5.2f}".format(one + two))
+#2
+#print("{:<20}....${:>0.2f}".format(one,two))
+#3
+#d
+#4
+#return (side**2)*6
+#5
+#print("The sum of{:0.0f}and {:0.0f} is {0.0f}".format(one,two,calcSum(one,two)))
+#6
+#global area
+#7
+#a
+#8
+#c
