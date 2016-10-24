9fe1a4b44e324879db3f448e0c3b8ef0530f33b1
mandylee900125 <LeeM3209@sduhsd.lan>
2016-10-24 10:04:37 -0700

    CHANGES

--- Lesson_04/Lab_4.1/Ex_03.py
+++ Lesson_04/Lab_4.1/Ex_03.py
@@ -4,7 +4,7 @@ n = int(input("Enter the number of times the loan in compunded per year:"))
 t = int(input("Enter number of years:"))
 
 def interest(p,r,n,t):
-    output= "{:10.2f}".format((p*(1+(r/n))**n*t)/t*12)
+    output= "{:10.2f}".format((p*((1+(r/n))**(n*t))/(t*12)))
     return output
 
 print ("Your payment amout is :", interest(p,r,n,t), "dollars")
--- Lesson_05/Lab_5.1/BMI part2.py
+++ Lesson_05/Lab_5.1/BMI part2.py
@@ -3,17 +3,21 @@ height = float(input("Please enter your height in inches."))
 bmi = 0
 condition = 0
 
-calcBMI():
-    bmi = weight/(height*height)* 703
-    if bmi <18.5:
-        condition = "underweight"
-    elif bmi <24.9:
-        condition = "normal"
-    elif bmi <29.9:
-        condition = "overweight"
-    elif bmi <34.9:
-        condition = "obese"
-    elif bmi <39.9:
-        condition = "very obese"
-    else:
-        condition = "morbidly obese"
+calcBMI(height,weight):
+    return weight/(height*height)* 703
+
+if bmi <=18.5:
+    conhsition = "underweight"
+elif bmi <=24.9:
+    condition = "normal"
+elif bmi <=29.9:
+    condition = "overweight"
+elif bmi <=34.9:
+    condition = "obese"
+elif bmi <=39.9:
+    condition = "very obese"
+else:
+    condition = "morbidly obese"
+
+print("Your BMI is:", calcBMI(height,weight))
+print("You are", condition)
