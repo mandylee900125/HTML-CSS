6515865f2fef9bec9b3dbf73dba71cf9b45ee80c
mandylee900125 <LeeM3209@sduhsd.lan>
2016-11-04 10:24:27 -0700

    added

--- Lesson_06/Lab_06.1/Ex_08 Song.py
+++ Lesson_06/Lab_06.1/Ex_08 Song.py
@@ -1,4 +1,15 @@
 word1 = "Na "
 word2 = "Hey "
 word3 = "Goodbye!"
-for i in range (
+output = ""
+
+def s(word, times,output):
+    for i in range (1,times+ 1):
+        output = output + word + " "
+        if i == times:
+            print(output)
+        
+s(word1,4,output)
+s(word1,4,output)
+s(word2,3,output)
+s(word3,1,output)
