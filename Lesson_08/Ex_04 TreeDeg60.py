e71cf526cac8cde3517582de759804388912955b
mandylee900125 <LeeM3209@sduhsd.lan>
2016-11-17 10:23:31 -0800

    changes

--- Lesson_08/Ex_04 TreeDeg60.py
+++ Lesson_08/Ex_04 TreeDeg60.py
@@ -1,11 +1,12 @@
+
 word = input("Enter a word ")
 start = 1
 stop = len(word)
 
 def tree(word,start,stop):
     if start <= stop:
-        print("{:>0}".format(word[0:start]))
-        start +1= 
+        print("{:>10}".format(word[0:start]))
+        start = start + 1
         tree(word,start,stop)
     else:
         return " "
