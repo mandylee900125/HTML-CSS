7886ff3c8557383c27f506fc30744493189b3265
mandylee900125 <LeeM3209@sduhsd.lan>
2016-11-28 10:25:04 -0800

    changes

---
+++ Lesson_09/Ex_03 Average Numbers.py
@@ -0,0 +1,20 @@
+numbers = []
+for i in range (0,100)
+    Numbers = i.randint
+print(Numbers)
+
+output = " "
+for v in numbers:
+    output = output + number + " "
+    
+print(output)
+print(" ")
+
+def average(nums):
+    int = 0
+    for l in nums
+        sum += num
+        return average/
+
+print("The average of the above number is...", average(numbers))
+    
