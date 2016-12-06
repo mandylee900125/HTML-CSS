6d3287be7ce4cd2514ebc54f498ad186decfeb42
mandylee900125 <LeeM3209@sduhsd.lan>
2016-12-06 10:23:43 -0800

    changes

--- Lesson_09/Lesson_09.1/Ex_05 Keep Composites.py
+++ Lesson_09/Lesson_09.1/Ex_05 Keep Composites.py
@@ -2,15 +2,16 @@ nums = [2,6,8,9,10,12,23,15,17,24,55,66,78,77,79]
 
 def gFactor(number):
     for l in range(2, number):
-        if number% = 0
+        if number % l == 0:
             return 1
-        else:
-            return 0
+    return 0
 def removePrimes(num):
     for n in num:
-        if gFactor(number) = 0
-            num.remove[number]
+        if gFactor(n) == 0:
+            num.remove(n)
             
 
-removePrimes(nums):
+removePrimes(nums)
+print(nums)
+
     
---
+++ Lesson_09/Lesson_09.1/Ex_06 Expression Solver.py
@@ -0,0 +1,4 @@
+eqn = input("Enter an equation")
+equation = word.split(" ")
+    if i < len(eqn):
+        if < len
