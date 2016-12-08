60e17d7bc36913306d6baa83ba64bdd07dcd22ad
mandylee900125 <LeeM3209@sduhsd.lan>
2016-12-08 10:24:55 -0800

    changes

--- Lesson_09/Lesson_09.1/Ex_06 Expression Solver.py
+++ Lesson_09/Lesson_09.1/Ex_06 Expression Solver.py
@@ -1,4 +1,31 @@
 eqn = input("Enter an equation")
 equation = word.split(" ")
-    if i < len(eqn):
-        if < len
+while i < len(eqn):
+    if i < len(eqn) and (eqn[i] == "*" or eqn [i] == "/"):
+        if equation[i] == "*":
+            eqn[i] == int(eqn[i-1])*int(eqn[i+1])
+        else:
+            eqn[i] = int(eqn[i-1])/ int(i+1)
+        equ.remove(i-1)
+        equ.remove(i)
+    i += 1
+i = 0
+while i < len(eqn):
+    if i < len(eqn) and (eqn[i] == "+" or eqn[i] == "-"):
+        if eqn[i] == "+":
+            eqn[i] == int(eqn[i-1]) + int(eqn[i+1])
+        else:
+            eqn[i] = int(eqn[i-1])-int(eqn[i+1])
+        eqn.remove(i-1)
+        eqn.remove(i)
+print(equation)
+        
+
+        
+
+
+
+
+
+            
+            
