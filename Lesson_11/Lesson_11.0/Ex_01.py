5a6b75f3bbbef65841fb6689b20b44941f66954f
mandylee900125 <LeeM3209@sduhsd.lan>
2017-01-12 10:24:56 -0800

    changes

---
+++ Lesson_11/Lesson_11.0/Ex_01.py
@@ -0,0 +1,11 @@
+class MilesPerHour:
+    def __init__ (self,d="",h="",m=""):
+        self.distance = d
+        self.hours = h
+        self.minutes = m
+        mph = 0
+    def setValues(self,newd
+
+    
+        
+    
