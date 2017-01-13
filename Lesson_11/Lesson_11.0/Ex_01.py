b663506a2079b1e51c7c237064dd4c4ae099ada2
mandylee900125 <LeeM3209@sduhsd.lan>
2017-01-13 10:24:38 -0800

    changes

--- Lesson_11/Lesson_11.0/Ex_01.py
+++ Lesson_11/Lesson_11.0/Ex_01.py
@@ -1,10 +1,43 @@
 class MilesPerHour:
-    def __init__ (self,d="",h="",m=""):
+    def __init__ (self,d,h,m):
         self.distance = d
         self.hours = h
         self.minutes = m
-        mph = 0
-    def setValues(self,newd
+        self.mph = 0
+        
+    def setValues(self,newDistance,newHours,newMinutes):
+        self.distance = newDistance
+        self.hours = newHours
+        self.minutes = newMinutes
+        self.mph = 0
+
+    def getDist(self):
+        return self.distance
+    
+    def getHours(self):
+        return self.hours
+    
+    def getMins(self):
+        return self.minutes
+    def getMPH(self):
+        self.mph = self.distance/((self.hours + self.minutes)/60.0)
+        return self.mph
+
+def main():
+    distance = int(input("Enter the distance: "))
+    hours = int(input("Enter the hours: "))
+    minutes = int(input("Enter the minutes: "))
+
+    user1 = MilesPerHour(distance,hours,minutes)
+    
+    print(user1.getDist(), " miles in ", user1.getHours(), "hours and ", user1.getMins(), " = ", user1.getMPH(), "mph.\n\n")
+
+    user1.setValues(10,2,0)
+
+    print(user1.getDist(), " miles in ", user1.getHours(), "hours and ", user1.getMins(), " = ", user1.getMPH(), "mph.")
+
+main()
+                
 
     
         
---
+++ Lesson_11/Lesson_11.0/Ex_02.py
@@ -0,0 +1,4 @@
+import math
+class Distance:
+    def __init__(param self,x1,y1,x2,y2):
+        
